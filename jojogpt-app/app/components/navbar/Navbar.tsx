52f5e4fc61ea5dcb6167f011806befb904767c0e
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-04-24 11:19:47 -0700

    remove old node app files

---
+++ jojogpt-app/app/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import Container from "../Container";
+
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="
+      py-4
+      border-b-[1px]
+      ">
+<Container>
+  whatever
+</Container>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- jojogpt-app/app/layout.tsx
+++ jojogpt-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from "next/font/google"
 
 import './globals.css'
+import Navbar from "./components/navbar/Navbar"
 
 export const metadata = {
   title: 'jojoGPT',
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+      </body>
     </html>
   )
 }
