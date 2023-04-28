ee224f15858e2940cf1799acbb229db5d4db3cbc
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-04-28 11:09:17 -0700

    implement Google and Github login functionality

--- jojogpt-app/app/components/modals/LoginModal.tsx
+++ jojogpt-app/app/components/modals/LoginModal.tsx
@@ -92,37 +92,14 @@ const LoginModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => { console.log("Google login") }}
+        onClick={() => { signIn('google')}}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => { console.log("Github login") }}
+        onClick={() => { signIn('github') }}
       />
-      <div
-        className="
-          text-neutral-500
-          text-center
-          mt-4
-          font-light
-        "
-      >
-        <div className="justify-center flex flex-row items-center gap-2">
-          <div>
-            Already have an account?
-          </div>
-          <div
-            onClick={registerModal.onClose}
-            className="
-              text-neutral-800
-              cursor-pointer
-              hover:underline
-          ">
-            Log in
-          </div>
-        </div>
-      </div>
     </div>
   );
 
--- jojogpt-app/app/components/modals/RegisterModal.tsx
+++ jojogpt-app/app/components/modals/RegisterModal.tsx
@@ -15,6 +15,7 @@ import Heading from "../Heading";
 import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -90,13 +91,13 @@ const RegisterModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => { console.log("Google login") }}
+        onClick={() => { signIn('google') }}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => { console.log("Github login") }}
+        onClick={() => { signIn('github')}}
       />
       <div
         className="
