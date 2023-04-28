1d7345d65e5f753ee00b0280c046fbff629a8f1c
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-04-28 10:53:38 -0700

    created SafeUser type to pass strings instead of DateTime

--- jojogpt-app/app/components/navbar/Navbar.tsx
+++ jojogpt-app/app/components/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import { User } from "@prisma/client";
 import Container from "../Container";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu";
+import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ( { currentUser }) => {
